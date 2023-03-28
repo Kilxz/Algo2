f70426a942d05477827eeb8fc901eede818d67ae
Kilxz <tomasrando@gmail.com>
2023-03-28 16:10:43 -0700

    Añadir pruebas

---
+++ pruebas/main.py
@@ -0,0 +1,22 @@
+"""
+L = [1,2,3,4,5,6,7,8]
+
+condition = L.index(8)
+print(condition)
+
+for i in "hola":
+    print(i)
+"""
+
+hola = "Holas"
+
+print(hola[-1])
+
+hola = hola[1:]
+print(hola)
+
+hola = "a"
+hola[1:]
+print(hola == "")
+print("asdcadasdas")
+print(hola)
