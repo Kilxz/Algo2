1650554392b95cb99504d48f064caec0a5b612b2
Kilxz <tomasrando@gmail.com>
2023-03-30 01:14:27 -0300

    Añadir pruebas para trie con linkedlist

--- practicas/tp-trie/main.py
+++ practicas/tp-trie/main.py
@@ -44,3 +44,52 @@ print(" ")
 print("Busco aps")
 print(search(T, "aps"))
 print(" ")
+
+
+
+#PRUEBAS PARA TRIE CON LINKEDLIST
+"""
+print("=========== PRUEBA Insert una palabra =========== ")
+insert(T, "Aps")
+print("Inserto aps")
+print(T.root)
+print(T.root.head.value.key)
+print(T.root.head.value.children.head.value.key)
+print(T.root.head.value.children.head.value.children.head.value.key)
+
+print(" ")
+print("=========== PRUEBA Insert dos palabras =========== ")
+insert(T, "App")
+print("Inserto App")
+print(T.root)
+print(T.root.head.value.key)
+print(T.root.head.value.children.head.value.key)
+print(T.root.head.value.children.head.value.children.head.value.key)
+
+print(" ")
+print("=========== PRUEBA isEndOfWord =========== ")
+insert(T, "a")
+print("Inserto a")
+print(T.root.head.value.isEndOfWord)
+print(T.root.head.value.children.head.value.isEndOfWord)
+
+print(" ")
+print("=========== PRUEBA search =========== ")
+
+print("Busco a")
+print(search(T, "a"))
+print(" ")
+
+print("Busco ap")
+print(search(T, "ap"))
+print(" ")
+
+print("Busco app")
+print(search(T, "app"))
+print(" ")
+
+print("Busco aps")
+print(search(T, "aps"))
+print(" ")
+
+"""
