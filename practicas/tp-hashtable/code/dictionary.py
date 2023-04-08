4c4e0b48c8d21732546761a371dfc077b938fc5b
Kilxz <tomasrando@gmail.com>
2023-04-07 23:41:43 -0300

    Inicio de hash

--- practicas/tp-hashtable/code/dictionary.py
+++ practicas/tp-hashtable/code/dictionary.py
@@ -1,13 +1,19 @@
 from algo1 import *
 
+class Dictionary:
+    head = None
+
 class DictionaryNode:
+    key = None
+    value = None
+    nextNode = None
     
 def dictionary(m):
     dict = Array(m, 0)
     return dict
 
 def hash(k, m):
-    return (k MOD m)
+    return (k % m)
 
 def insert(D, key, value):
     k = hash(k, len(D))
