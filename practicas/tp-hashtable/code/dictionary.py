efe1f86b84ecad1f72b651e398a2e32d1ed9b59c
Kilxz <tomasrando@gmail.com>
2023-04-13 01:00:20 -0300

    Implementar search, delete e insert

--- practicas/tp-hashtable/code/dictionary.py
+++ practicas/tp-hashtable/code/dictionary.py
@@ -1,19 +1,49 @@
 from algo1 import *
-
-class Dictionary:
-    head = None
-
-class DictionaryNode:
-    key = None
-    value = None
-    nextNode = None
+import linkedlist
     
 def dictionary(m):
-    dict = Array(m, 0)
+    dict = Array(m, linkedlist.LinkedList())
     return dict
 
 def hash(k, m):
     return (k % m)
 
 def insert(D, key, value):
-    k = hash(k, len(D))
+    k = hash(key, len(D))
+    element = (key, value)
+    if D[k] == None:
+        newList = linkedlist.LinkedList()
+        linkedlist.add(newList, element)
+        D[k] = newList
+    else:
+        linkedlist.add(D[k], element)
+    return D
+    
+def search(D, key):
+    k = hash(key, len(D))
+    if D[k] == None:
+        return None
+    
+    currentNode = D[k].head
+    while currentNode != None:
+        if currentNode.value[0] == key:
+            return currentNode.value[1]
+        currentNode = currentNode.nextNode
+    return None
+
+def delete(D, key):
+    k = hash(key, len(D))
+    if D[k] == None:
+        return D
+    if D[k].head.value[0] == key:
+        D[k].head = D[k].head.nextNode
+        if D[k].head == None:
+            D[k] = None
+        return D
+    
+    currentNode = D[k].head
+    while currentNode.nextNode != None:
+        if currentNode.nextNode.value[0] == key:
+            currentNode.nextNode = currentNode.nextNode.nextNode
+            return D
+        currentNode = currentNode.nextNode
