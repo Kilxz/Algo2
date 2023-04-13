c4ab6b190358bb00c9adc8fa3ca0fb7310b450e6
Kilxz <tomasrando@gmail.com>
2023-04-13 01:00:35 -0300

    -

--- pruebas/main.py
+++ pruebas/main.py
@@ -1,4 +1,5 @@
 import math
+from algo1 import *
 """
 L = [1,2,3,4,5,6,7,8]
 
@@ -51,7 +52,7 @@ L = [5, 28, 19, 15, 20,  33, 12, 17, 10]
 
 for i in L:
     print(i % 9)
-"""
+
 def hashMult(k, m, A):
     return m * ((k * (math.sqrt(5) -1) / 2) % 1)
 
@@ -63,3 +64,8 @@ for i in L:
     lista.append(hashMult(i, m, A))
 
 print(lista)
+"""
+
+dict = Array(3, 0)
+
+print(dict[0])
